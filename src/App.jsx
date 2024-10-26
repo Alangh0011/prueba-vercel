e33b429b4d40c30e82471cb26c388f3e3e3b3820
Alan Gomez <agomezh1901@alumno.ipn.mx>
2024-10-26 06:09:24 -0600

    Prueba en vercel

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Navbar'
+import Chatbot from './Chatbot'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Chatbot/>
+    </div>
+  )
+}
+
+export default App
